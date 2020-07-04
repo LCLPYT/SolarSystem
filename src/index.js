11ace148a8834689db36a77782ef5032d395a052
LCLP <lukas@lclpnet.work>
2020-07-04 13:19:21 +0200

    Webpack dev server and sun added

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import './style.css';
 import * as THREE from 'three';
 
 let scene, camera, renderer;
-let cube;
+let sun;
 
 init();
 animate();
@@ -33,32 +33,31 @@ function init() {
   // Hinzufügen des HTML5 Leinwand Objektes (canvas)
   document.body.appendChild(renderer.domElement);
 
-  /** Darstellung eines Würfels **/
+  /** Darstellung der Sonne **/
+  addSun();
 
-  // Definieren eines Geometrieobjektes, welches alle Eckpunkte, sowie Flächen dieser speichert. BoxGeometry ist in dem Fall die Geometrie für einen Würfel.
-  let cubeGeometry = new THREE.BoxGeometry();
-  // Definieren eines Materials, welches für das Objekt benutzt wird. Dieses Material sorgt für die Farbe des dargestellten Objektes. In diesem Fall sorgt dieses Material dafür, dass der Würfel grün dargestellt wird.
-  let cubeMaterial = new THREE.MeshLambertMaterial({
-    color: 0x00ff00
-  });
-  // Definieren eines Meshes. Dieses Objekt speichert eine Geometrie und ein dazugehöriges Material. Dieses Objekt repräsentiert im Moment einen grünen Würfel.
-  cube = new THREE.Mesh(cubeGeometry, cubeMaterial);
-
-  // Hinzufügen des Würfels zur Szene.
-  scene.add(cube);
-
-  // Hinzufügen von Lichtquellen
-  // Ambient Light kommt von überall, es ist sozusagen ein Minimumlichtwert.
-  let ambientLight = new THREE.AmbientLight(0xffffff, 0.1);
-  scene.add(ambientLight);
-
-  // Point Light kommt von einem bestimmten Punkt. Dies ist realitätsnah und z.B. wie eine Lampe vorzustellen.
-  let pointLight = new THREE.PointLight(0xffffff, 0.5);
-  pointLight.position.z += 10;
-  scene.add(pointLight);
-
-  // Der Würfel ist im Moment im Koordinatenursprung, genauso, wie die Kamera. Deswegen würde man den Würfel nicht sehen. Mit dieser Anweisung wird die Position der Kamera auf der z-Achse um 5 Einheiten bewegt, damit man den Würfel sehen kann.
+  // Die Kamera aus dem Koordinatenursprung bewegen, da sie sonst in der Sonne stecken würde.
   camera.position.z = 5;
+}
+
+function addSun() {
+  // Definieren eines Geometrieobjektes, welches alle Eckpunkte, sowie Flächen dieser speichert. SphereGeometry ist in dem Fall die Geometrie für eine Kugel.
+  let sunGeometry = new THREE.SphereGeometry(
+    1, //Radius
+    64, // widthSegments - Anzahl der horizontalen Segmente der Kugel
+    64 // heightSegments - Anzahl der vertikalen Segmente der Kugel
+  );
+
+  // Definieren eines Materials, welches für das Objekt benutzt wird. Dieses Material sorgt für die Farbe des dargestellten Objektes. In diesem Fall sorgt dieses Material dafür, dass die Sonne gelb dargestellt wird.
+  let sunMaterial = new THREE.MeshStandardMaterial({
+    color: 0xFDB813, // Farbe der Sonne
+    emissive: 0xFDB813, // Sonne soll als Lichtquelle agieren, deshalb emmitiert sie Licht
+    emissiveIntensity: 1 // Stärke der Emmitierung (TODO test)
+  });
+  // Definieren eines Meshes. Dieses Objekt speichert eine Geometrie und ein dazugehöriges Material. Dieses Objekt repräsentiert im Moment die Sonne.
+  sun = new THREE.Mesh(sunGeometry, sunMaterial);
+  // Hinzufügen der Sonne zur Szene.
+  scene.add(sun);
 }
 
 /**
@@ -67,10 +66,6 @@ function init() {
  */
 function animate() {
   requestAnimationFrame(animate); // Diese Anweisung sorgt dafür, dass die Funktion wiederholt aufgerufen wird. Dabei wird auch eine bestimmte Zeit gewartet um der Wiederholfrequenz des Bildschirms gerecht zu werden.
-
-  // Hier wird die Rotation des Würfels verändert, damit der Würfel nicht nur immer in der selben Position abgebildet wird.
-  cube.rotation.x += 0.01;
-  cube.rotation.y += 0.02;
 
   // Mit dieser Anweisung bildet der Renderer die Szene auf der Leinwand ab.
   renderer.render(scene, camera);
