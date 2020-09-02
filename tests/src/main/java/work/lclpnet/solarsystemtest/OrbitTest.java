bc99b0b83772f85be3d55cae256d4acdb5ab76e2
LCLP <lukas@lclpnet.work>
2020-09-02 12:12:15 +0200

    stable ellipsis

--- tests/src/main/java/work/lclpnet/solarsystemtest/OrbitTest.java
+++ tests/src/main/java/work/lclpnet/solarsystemtest/OrbitTest.java
@@ -25,12 +25,12 @@ public class OrbitTest {
         double vx = 1.942629911583868E-03, vy = 1.511819668505303E-02, vz = 2.691519759640370E-04;*/
         final double M = 5.97E+24;
         double x = 0, y = 6.371E+6;
-        double vx = 1, vy = 0;
+        double vx = 5000, vy = 0;
         double t = 0, dt = 1;
 
         System.out.printf("x=%s y=%s\n", x, y);
 
-        int it = 1000;
+        int it = 5000;
         double[] xData = new double[it], yData = new double[it];
 
         for (int i = 0; i < it; i++) {
@@ -54,7 +54,7 @@ public class OrbitTest {
 
         final XYChart chart = new XYChartBuilder().width(600).height(400).title("Orbit Chart").xAxisTitle("X").yAxisTitle("Y").build();
         chart.getStyler().setDefaultSeriesRenderStyle(XYSeries.XYSeriesRenderStyle.Scatter);
-        chart.getStyler().setLegendPosition(Styler.LegendPosition.InsideSW);
+        chart.getStyler().setLegendPosition(Styler.LegendPosition.InsideNE);
 
         XYSeries marsSeries = chart.addSeries("Satellit", xData, yData);
         chart.addSeries("Erde", new int[] {0}, new int[] {0});
