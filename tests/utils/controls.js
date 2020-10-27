f723191292c6406bba2a5f6e7258706b6532d6bc
LCLP <lukas@lclpnet.work>
2020-10-27 21:02:39 +0100

    modularize tests

--- tests/utils/controls.js
+++ tests/utils/controls.js
@@ -1,17 +1,19 @@
+import * as CONSTANTS from './constants.js';
+
 const btnZoomIn = document.getElementById("zoomIn"), btnZoomOut = document.getElementById("zoomOut");
 const timeScaleInput = document.getElementById("timeScaleInput");
 
 btnZoomIn.addEventListener("click", () => {
-    let pixel = 1 / auPerPixel;
-    auPerPixel = 1 / (pixel + 3);
+    let pixel = 1 / CONSTANTS.auPerPixel;
+    CONSTANTS.setAuPerPixel(1 / (pixel + 3));
 });
 btnZoomOut.addEventListener("click", () => {
-    let pixel = 1 / auPerPixel;
+    let pixel = 1 / CONSTANTS.auPerPixel;
     if(pixel == 3) return;
-    auPerPixel = 1 / (pixel - 3);
+    CONSTANTS.setAuPerPixel(1 / (pixel - 3));
 });
 
-timeScaleInput.value = secondMultiplier / 60;
+timeScaleInput.value = CONSTANTS.secondMultiplier / 60;
 timeScaleInput.addEventListener ("change", function () {
-    secondMultiplier = Number(timeScaleInput.value) * 60;
+    CONSTANTS.setSecondMultiplier(Number(timeScaleInput.value) * 60);
 });
