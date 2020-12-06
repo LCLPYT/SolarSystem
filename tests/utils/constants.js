8b10b6fc3ce709b4b6aec19127552a45f99671e9
LCLP <35002345+LCLPYT@users.noreply.github.com>
2020-12-06 19:55:47 +0100

    Update constants.js

--- tests/utils/constants.js
+++ tests/utils/constants.js
@@ -3,7 +3,7 @@ export const canvas = (document.getElementById('canvas'));
 /** @type {CanvasRenderingContext2D} */
 export const ctx = (canvas.getContext('2d'));
 
-export const grav = 6.67430E-11; // m^3*km^-2*s^-1
+export const grav = 6.67430E-11; // m^3*kg^-1*s^-2
 
 export let auPerPixel = 1 / 15; // 1 AE pro 15 pixel
 export let secondMultiplier = 6000; // 1 Sekunde Echtzeit entspricht 6000 Sekunden in der Simulation
