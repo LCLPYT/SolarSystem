c03ce301f09f3b938736786d4850d41bd8b7a584
LCLP <lukas@lclpnet.work>
2020-12-19 15:08:56 +0100

    refactor

--- src/ts/OrbitBody.ts
+++ src/ts/OrbitBody.ts
@@ -43,37 +43,39 @@ export class OrbitBody extends Body {
     feedOrbitPosition(position: Vector3) {
         let i = this.vertexIndex++;
         if(this.vertexIndex >= ORBIT_MAX_VERTICES * 2) this.vertexIndex = 0;
-        
-        const geometry1 = <BufferGeometry> this.orbit[0].geometry;
-        const geometry2 = <BufferGeometry> this.orbit[1].geometry;
-        const positionAttribute1 = <BufferAttribute> geometry1.attributes.position;
-        const positionAttribute2 = <BufferAttribute> geometry2.attributes.position;
-        
+
+        let part: number;
+        let other: number;
         if(i < ORBIT_MAX_VERTICES) {
-            if(this.secondOrbitUsed) {
-                if (i === 0) {
-                    i += 1;
-                    positionAttribute1.setXYZ(0, positionAttribute2.getX(ORBIT_MAX_IDX), positionAttribute2.getY(ORBIT_MAX_IDX), positionAttribute2.getZ(ORBIT_MAX_IDX));
-                }
-                geometry2.setDrawRange(i + 1, ORBIT_MAX_VERTICES - i - 1);
-            }
-            
-            positionAttribute1.setXYZ(i, position.x, position.y, position.z);
-            geometry1.setDrawRange(0, i + 1);
-            positionAttribute1.needsUpdate = true;
+            part = 0;
+            other = 1;
         } else {
+            part = 1;
+            other = 0;
+        }
+
+        const currentGeometry = <BufferGeometry> this.orbit[part].geometry;
+        const otherGeometry = <BufferGeometry> this.orbit[other].geometry;
+        const currentPositionAttribute = <BufferAttribute> currentGeometry.attributes.position;
+        const otherPositionAttribute = <BufferAttribute> otherGeometry.attributes.position;
+
+        if(part === 1) {
             this.secondOrbitUsed = true;
             i -= ORBIT_MAX_VERTICES;
-            if(i === 0) {
-                i += 1;
-                positionAttribute2.setXYZ(0, positionAttribute1.getX(ORBIT_MAX_IDX), positionAttribute1.getY(ORBIT_MAX_IDX), positionAttribute1.getZ(ORBIT_MAX_IDX));
-            }
-            positionAttribute2.setXYZ(i, position.x, position.y, position.z);
-            geometry2.setDrawRange(0, i + 1);
-            positionAttribute2.needsUpdate = true;
-            
-            geometry1.setDrawRange(i + 1, ORBIT_MAX_VERTICES - i - 1);
         }
+
+        let secondOrSecondUsed = part === 1 || this.secondOrbitUsed;
+        if(i === 0 && secondOrSecondUsed) {
+            i += 1;
+            currentPositionAttribute.setXYZ(0, otherPositionAttribute.getX(ORBIT_MAX_IDX), otherPositionAttribute.getY(ORBIT_MAX_IDX), otherPositionAttribute.getZ(ORBIT_MAX_IDX));
+        }
+
+        currentPositionAttribute.setXYZ(i, position.x, position.y, position.z);
+        currentPositionAttribute.needsUpdate = true;
+        currentGeometry.setDrawRange(0, i + 1);
+
+        if(secondOrSecondUsed) 
+            otherGeometry.setDrawRange(i + 1, ORBIT_MAX_VERTICES - i - 1);
     }
     
 }
